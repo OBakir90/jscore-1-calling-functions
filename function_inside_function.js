9867f90ffc5865f2a4e20e2983ca9f6bac661925
OBakir90 <53667329+OBakir90@users.noreply.github.com>
2020-04-20 01:38:45 +0100

    first commit

---
+++ function_inside_function.js
@@ -0,0 +1,32 @@
+// 1. Write a
+// function that returns the year someone is born given their age as input
+// 2. Using the answer from step 1, write a
+// function that takes someone 's name and age as input and returns a string that states the person'
+// s name and year they were born in a sentence
+
+
+
+
+function getBirthYear() {
+    const userAge = document.getElementById("input_age").value;
+    const date = new Date()
+    const currentYear = date.getFullYear();
+    return (currentYear - userAge);
+}
+
+function userData() {
+    const name = document.getElementById("input_name").value;
+
+    document.getElementById("result").innerHTML = `${name} was born in ${getBirthYear()}`;
+
+    console.log(`${name} was born in ${getBirthYear()}`);
+
+    console.log(name);
+}
+
+
+// var customerName = prompt("Please enter your name")
+
+// function findingBirthYear() {
+
+// }
