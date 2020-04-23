053ab0d8d473e2f6ea8ed6ec03e4c93c5360f615
OBakir90 <53667329+OBakir90@users.noreply.github.com>
2020-04-23 13:54:09 +0100

    added world cup function

--- function_inside_function.js
+++ function_inside_function.js
@@ -7,7 +7,12 @@
 const userAge = document.getElementById("input_age");
 const userName = document.getElementById("input_name");
 const results = document.getElementById("result");
-const classContainer = document.querySelector("#maincontainer");
+const classContainer = document.getElementById("maincontainer");
+const worldCup = document.getElementById("world_cup");
+
+
+
+
 
 function getBirthYear() {
     const date = new Date();
@@ -15,25 +20,50 @@ function getBirthYear() {
     return currentYear - userAge.value;
 }
 
+function getWorldCupYears(year) {
+    if ((year - 1930) % 4 === 0) {
+        return year;
+    }
+}
+
+function getNumberWC(year) {
+    if ((year - 1930) % 4 === 0) {
+        return (year - 1930) / 4;
+    }
+}
+
+console.log(getWorldCupYears(getBirthYear()));
+
 function getUserInfo() {
     results.innerHTML = "";
     classContainer.className = "container";
-    if (getBirthYear() > 2010) {
-        classContainer.className = "kid";
-        results.innerHTML = `Hi ${
-        userName.value
-    }, you were born in ${getBirthYear()}`;
-    } else if (getBirthYear() > 2002) {
-        classContainer.className = "teenager";
-        results.innerHTML = `Hi ${
-        userName.value
-    }, you were born in ${getBirthYear()}`;
+    if (getBirthYear() > 2020) {
+        results.innerHTML = `Not yet born`;
+    } else if (getBirthYear() < 1900) {
+        results.innerHTML = `Still cant be alive`;
     } else {
-        classContainer.className = "adult";
-        results.innerHTML = `Hi ${
-        userName.value
-    }, you were born in ${getBirthYear()}`;
+        if (getBirthYear() > 2010) {
+            classContainer.className = "kid";
+            results.innerHTML = `Hi ${
+            userName.value
+        }, you were born in ${getBirthYear()}`;
+        } else if (getBirthYear() > 2002) {
+            classContainer.className = "teenager";
+            results.innerHTML = `Hi ${
+            userName.value
+        }, you were born in ${getBirthYear()}`;
+        } else {
+            classContainer.className = "adult";
+            results.innerHTML = `Hi ${
+            userName.value
+        }, you were born in ${getBirthYear()}`;
+        }
+        if (getBirthYear() === 1944 && getBirthYear() === 1948) {
+            worldCup.innerHTML = "World Cup was not held because of the Second World War"
+        } else if (getBirthYear() == getWorldCupYears(getBirthYear())) {
+            worldCup.innerHTML = `${getNumberWC(getBirthYear())}. World Cup held`
+        }
+        userAge.value = "";
+        userName.value = "";
     }
-    userAge.value = "";
-    userName.value = "";
 }
